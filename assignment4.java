3bdbed04c4e5678a217e1525275dad82cc3be0e5
Ritvik Gupta <ritvikgupta199@gmail.com>
2020-12-23 16:15:47 +0530

    Added ordering in Components

--- assignment4.java
+++ assignment4.java
@@ -3,7 +3,6 @@ import java.util.ArrayList;
 import java.util.Vector;
 import java.util.LinkedList;
 import java.util.Map;
-import java.util.Random;
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -12,25 +11,25 @@ public class assignment4 {
         ArgParser argparser = new ArgParser();
         argparser.parse(args);
 
-        Graph graph = new Graph("nodes.csv", "edges.csv");
+        Graph graph = new Graph(argparser.nodesFile, argparser.edgesFile);
         graph.makeGraph();
 
         if (argparser.function.equals("average")) {
             graph.calcAverage();
         } else if (argparser.function.equals("rank")) {
             graph.calcRank();
-        } else if(argparser.function.equals("independent_storylines_dfs")){
+        } else if (argparser.function.equals("independent_storylines_dfs")) {
             graph.calcIndependentStorylines();
-        }else {
+        } else {
             System.out.println("Wrong arugument");
         }
     }
 }
 
 class Graph {
-    HashMap<String, Vector<Pair>> graph;
+    HashMap<String, Vector<Pair<Integer, String>>> graph;
     HashMap<String, Boolean> vis;
-    ArrayList<ArrayList<String>> components;
+    ArrayList<ComparableArrayList<String>> components;
     String nodesFile;
     String edgesFile;
 
@@ -48,8 +47,9 @@ class Graph {
     }
 
     void calcAverage() {
-        int sum = 0, count = 0;
-        for (Map.Entry<String, Vector<Pair>> mapElement : graph.entrySet()) {
+        int sum = 0;
+        int count = 0;
+        for (Map.Entry<String, Vector<Pair<Integer, String>>> mapElement : graph.entrySet()) {
             sum += (mapElement.getValue().size());
             count++;
         }
@@ -58,48 +58,53 @@ class Graph {
     }
 
     void calcRank() {
-        ArrayList<Pair> ranklist = new ArrayList<>();
-        for (Map.Entry<String, Vector<Pair>> mapElement : graph.entrySet()) {
+        ArrayList<Pair<Integer, String>> ranklist = new ArrayList<>();
+        for (Map.Entry<String, Vector<Pair<Integer, String>>> mapElement : graph.entrySet()) {
             int sum = 0;
-            for (Pair p : mapElement.getValue()) {
-                sum += p.second;
+            for (Pair<Integer, String> p : mapElement.getValue()) {
+                sum += p.first;
             }
-            ranklist.add(new Pair(mapElement.getKey(), sum));
+            ranklist.add(new Pair<Integer, String>(sum, mapElement.getKey()));
         }
-        QuickSort.sort(ranklist);
+        QuickSort.revSort(ranklist);
         int n = ranklist.size();
         for (int i = 0; i < n; i++) {
-            System.out.print(ranklist.get(i).first);
+            System.out.print(ranklist.get(i).second);
             if (i != n - 1) {
                 System.out.print(",");
             }
         }
+        System.out.print("\n");
     }
 
     void DFS(String v, int count) {
-        Vector<Pair> adj = graph.get(v);
+        Vector<Pair<Integer, String>> adj = graph.get(v);
         components.get(count).add(v);
         vis.put(v, true);
         for (int i = 0; i < adj.size(); i++) {
-            Pair u = adj.get(i);
-            if (vis.get(u.first) != null && !vis.get(u.first)) {
-                DFS(u.first, count);
+            Pair<Integer, String> u = adj.get(i);
+            if (vis.get(u.second) != null && !vis.get(u.second)) {
+                DFS(u.second, count);
             }
         }
     }
 
     void calcIndependentStorylines() {
         int count = 0;
-        for (Map.Entry<String, Vector<Pair>> mapElement : graph.entrySet()) {
+        for (Map.Entry<String, Vector<Pair<Integer, String>>> mapElement : graph.entrySet()) {
             boolean v = vis.get(mapElement.getKey());
             if (!v) {
-                components.add(new ArrayList<>());
+                components.add(new ComparableArrayList<>());
                 DFS(mapElement.getKey(), count);
                 count++;
             }
         }
         for (int i = 0; i < components.size(); i++) {
-            ArrayList<String> list = components.get(i);
+            QuickSort.revSort(components.get(i).list);
+        }
+        QuickSort.revSort(components);
+        for (int i = 0; i < components.size(); i++) {
+            ComparableArrayList<String> list = components.get(i);
             int n = list.size();
             for (int j = 0; j < n; j++) {
                 System.out.print(list.get(j));
@@ -128,11 +133,46 @@ class Graph {
         while (reader.hasNext()) {
             String s = reader.nextLine();
             String[] row = s.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
-            graph.get(row[0]).add(new Pair(row[1], Integer.parseInt(row[2])));
-            graph.get(row[1]).add(new Pair(row[0], Integer.parseInt(row[2])));
+            graph.get(row[0]).add(new Pair<>(Integer.parseInt(row[2]), row[1]));
+            graph.get(row[1]).add(new Pair<>(Integer.parseInt(row[2]), row[0]));
         }
     }
 
+}
+
+class ComparableArrayList<T extends Comparable<T>> implements Comparable<ComparableArrayList<T>> {
+    ArrayList<T> list;
+
+    ComparableArrayList() {
+        this.list = new ArrayList<>();
+    }
+
+    int size() {
+        return list.size();
+    }
+
+    boolean add(T e) {
+        return list.add(e);
+    }
+
+    T get(int index) {
+        return list.get(index);
+    }
+
+    public int compareTo(ComparableArrayList<T> l) {
+        int n1 = this.list.size();
+        int n2 = l.list.size();
+        if (n1 != n2) {
+            return n1 - n2;
+        } else {
+            for (int i = 0; i < n1; i++) {
+                if (list.get(i).compareTo(l.list.get(i)) != 0) {
+                    return list.get(i).compareTo(l.list.get(i));
+                }
+            }
+            return 0;
+        }
+    }
 }
 
 class ArgParser {
@@ -184,26 +224,25 @@ class FileReader {
     }
 }
 
-class Pair implements Comparable<Pair> {
-    public String first;
-    public Integer second;
+class Pair<K extends Comparable<K>, V extends Comparable<V>> implements Comparable<Pair<K, V>> {
+    public K first;
+    public V second;
 
-    Pair(String first, int second) {
+    Pair(K first, V second) {
         this.first = first;
         this.second = second;
     }
 
-    public int compareTo(Pair p) {
-        int res = this.second.compareTo(p.second);
+    public int compareTo(Pair<K, V> p) {
+        int res = this.first.compareTo(p.first);
         if (res == 0) {
-            res = this.first.compareTo(p.first);
+            res = this.second.compareTo(p.second);
         }
         return res;
     }
 }
 
 class QuickSort<T> {
-    private static Random rand = new Random();
 
     public static <T extends Comparable<T>> int partition(ArrayList<T> arr, int l, int r, int p) {
         T pivot = arr.get(p);
@@ -227,14 +266,14 @@ class QuickSort<T> {
 
     public static <T extends Comparable<T>> void quicksort(ArrayList<T> arr, int l, int r) {
         if (l < r) {
-            int pivot = rand.nextInt(r - l + 1) + l;
+            int pivot = l + (int) ((r - l + 1) * Math.random());
             int p = partition(arr, l, r, pivot);
             quicksort(arr, l, p);
             quicksort(arr, p + 1, r);
         }
     }
 
-    public static <T extends Comparable<T>> void sort(ArrayList<T> arr) {
+    public static <T extends Comparable<T>> void revSort(ArrayList<T> arr) {
         quicksort(arr, 0, arr.size() - 1);
     }
 }
